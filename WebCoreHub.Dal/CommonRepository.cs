0e1615181b77b6a3ff29571bc27a92e085236782
Anjo Tadena <tadena.anjo@gmail.com>
2022-08-31 22:30:41 +0800

    Create CommonRepository

---
+++ WebCoreHub.Dal/CommonRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebCoreHub.Dal
+{
+    public class CommonRepository<T> : ICommonRepository<T> where T : class
+    {
+        private readonly WebCoreHubDbContext _dbContext;
+        
+        private DbSet<T> table;
+
+        public CommonRepository(WebCoreHubDbContext context)
+        {
+            _dbContext = context;
+            table = _dbContext.Set<T>();
+        }
+
+        public void Delete(T item)
+        {
+            table.Remove(item);
+        }
+
+        public List<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetDetails(int id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T item)
+        {
+            table.Add(item);
+        }
+
+        public int SaveChanges()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            table.Attach(item);
+            _dbContext.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
---
+++ WebCoreHub.Dal/ICommonRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace WebCoreHub.Dal
+{
+    public interface ICommonRepository<T>
+    {
+        List<T> GetAll();
+
+        T GetDetails(int id);
+
+        void Insert(T item);
+
+        void Update(T item);
+
+        void Delete(T item);
+
+        int SaveChanges();
+    }
+}
