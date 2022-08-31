b093d56c5258d1cec3bbf36b2d848a35d6fd4d1d
Anjo Tadena <tadena.anjo@gmail.com>
2022-08-31 19:31:24 +0800

    Add project data access layer(DAL)

---
+++ WebCoreHub.Dal/Migrations/20220831105213_CreateNewDatabase.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCoreHub.Dal.Migrations
+{
+    public partial class CreateNewDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Zipcode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Skillsets = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Avatar = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventCode = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    EventName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Fees = table.Column<decimal>(type: "Decimal(18,2)", nullable: false),
+                    SeatsFilled = table.Column<int>(type: "int", nullable: false),
+                    Logo = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    RoleDescription = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "Address", "Avatar", "City", "Country", "Email", "EmployeeName", "Phone", "Skillsets", "Zipcode" },
+                values: new object[,]
+                {
+                    { 1, "East Blue", "/images/john-doe.png", "Cebu", "ph", "john.doe@mailinato.com", "John Doe", "09979232212", "DBA", "8710" },
+                    { 2, "West Blue", "/images/jane-doe.png", "Cebu", "ph", "jane.doe@mailinato.com", "Jane Doe", "09979217813", "Secretary", "9900" },
+                    { 3, "North Blue", "/images/joe-doe.png", "Cebu", "ph", "joe.doe@mailinato.com", "Joe Doe", "09979213663", "Programmer", "7091" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "RoleDescription", "RoleName" },
+                values: new object[,]
+                {
+                    { 1, "Employee of WebCoreHub Organization", "Employee" },
+                    { 2, "HR of WebCoreHub Organization", "HR" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
---
+++ WebCoreHub.Dal/WebCoreHubDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCoreHub.Models;
+
+namespace WebCoreHub.Dal
+{
+    public class WebCoreHubDbContext : DbContext
+    {
+        public WebCoreHubDbContext()
+        {
+
+        }
+
+        public WebCoreHubDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Data Source = localhost; Initial Catalog = WebCoreApiDb; Integrated Security = true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee() {  EmployeeId = 1, EmployeeName = "John Doe", Address = "East Blue", City = "Cebu", Country = "Ph", Zipcode = "8710", Phone = "09979232212", Email = "john.doe@mailinato.com", Skillsets = "DBA", Avatar = "/images/john-doe.png"},
+                new Employee() {  EmployeeId = 2, EmployeeName = "Jane Doe", Address = "West Blue", City = "Cebu", Country = "Ph", Zipcode = "9900", Phone = "09979217813", Email = "jane.doe@mailinato.com", Skillsets = "Secretary", Avatar = "/images/jane-doe.png"},
+                new Employee() {  EmployeeId = 3, EmployeeName = "Joe Doe", Address = "North Blue", City = "Cebu", Country = "Ph", Zipcode = "7091", Phone = "09979213663", Email = "joe.doe@mailinato.com", Skillsets = "Programmer", Avatar = "/images/joe-doe.png" }
+            );
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role() { RoleId = 1, RoleName = "Employee", RoleDescription = "Employee of WebCoreHub Organization"},
+                new Role() { RoleId = 2, RoleName = "HR", RoleDescription = "HR of WebCoreHub Organization"}
+            );
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
