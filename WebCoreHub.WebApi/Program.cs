130445c4b8772f624cae2b2d2e2a578fc5129425
Anjo Tadena <tadena.anjo@gmail.com>
2022-09-02 06:27:59 +0800

    AddScoped for TokenManager

--- WebCoreHub.WebApi/Program.cs
+++ WebCoreHub.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebCoreHub.Dal;
 using WebCoreHub.Models;
+using WebCoreHub.WebApi.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,10 @@ builder.Services.AddDbContext<WebCoreHubDbContext>(options => options.UseSqlServ
 builder.Services.AddTransient<ICommonRepository<Employee>, CommonRepository<Employee>>();
 builder.Services.AddTransient<ICommonRepository<Event>, CommonRepository<Event>>();
 builder.Services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
+// AddScoped()
+// This methods creates a Scoped service. A new instance of a Scoped service created one per request within the scope.
+// For Example, in a web application creates 1 instance per each http request but uses the same instance in the other calls that same web request.
+builder.Services.AddScoped<ITokenManager, TokenManager>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
