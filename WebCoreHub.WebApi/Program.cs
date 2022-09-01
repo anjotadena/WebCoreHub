baa490f8052c79eb6f39a706d479e595ebebbf2f
Anjo Tadena <tadena.anjo@gmail.com>
2022-09-02 06:50:35 +0800

    Add JWT middleware

--- WebCoreHub.WebApi/Program.cs
+++ WebCoreHub.WebApi/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebCoreHub.Dal;
 using WebCoreHub.Models;
 using WebCoreHub.WebApi.Jwt;
@@ -17,6 +20,19 @@ builder.Services.AddTransient<IAuthenticationRepository, AuthenticationRepositor
 // This methods creates a Scoped service. A new instance of a Scoped service created one per request within the scope.
 // For Example, in a web application creates 1 instance per each http request but uses the same instance in the other calls that same web request.
 builder.Services.AddScoped<ITokenManager, TokenManager>();
+
+// builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["WebCoreHubJWT:Secret"])),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
